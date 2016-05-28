18928634383215dd6aeb89755d18b40126e5000e
Makis Maropoulos <makis@ideopod.com>
2016-05-28 21:51:33 +0300

    Initial commit for the basic package

---
+++ basic/backend/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+)
+
+func init() {
+	// set the configs
+	configuration := iris.Config()
+
+	configuration.Render.Template.Directory = "../frontend/templates"
+	configuration.Render.Template.Layout = "layout.html"
+
+	// enable websocket server by setting its endpoint to 127.0.0.1/ws
+	configuration.Websocket.Endpoint = "/ws"
+}
+
+func main() {
+	// set the favicon
+	iris.Favicon("../frontend/public/images/favicon.ico", "/favicon.ico")
+
+	// set static folder(s)
+	iris.Static("/public", "../frontend/public", 1)
+
+	makeRouter()
+
+	// start the server
+	err := iris.ListenWithErr("127.0.0.1:80")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func makeRouter() {
+	// set the routes
+	iris.Handle("GET", "/", Index())
+	// iris.Get("/", Index().Serve)
+}
